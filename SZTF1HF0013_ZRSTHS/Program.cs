95e3301b67a710df2f41585d89d81ca668028902
KovagoD <kovago.david1103@gmail.com>
2022-04-29 10:47:12 +0200

    Initial commit

---
+++ SZTF1HF0013_ZRSTHS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SZTF1HF0013_ZRSTHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Convert.ToInt64(Console.ReadLine()).ToString("X");
+            string output = input.Replace("0", "O").Replace("1", "I");
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (Char.IsNumber(output[i]))
+                {
+                    output = "error";
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
